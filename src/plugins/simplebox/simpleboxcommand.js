221fe12d52dde2112128533c3d85aa7ca1457efc
Bendel <b.bendeliani@gmail.com>
2020-01-28 19:31:39 +0400

    Update.

--- src/plugins/simplebox/simpleboxcommand.js
+++ src/plugins/simplebox/simpleboxcommand.js
@@ -1,97 +1,112 @@
 import Command from "@ckeditor/ckeditor5-core/src/command";
 import Swal from "sweetalert2";
+
 export default class SimpleBoxCommand extends Command {
-  execute() {
-    this.editor.model.change(writer => {
-      // Insert <simpleBox>*</simpleBox> at the current selection position
-      // in a way that will result in creating a valid model structure.
-      console.log("ager movida");
 
-      createSimpleBox(writer, this.editor).then(x => {
-        this.editor.model.insertContent(x);
-      });
-    });
-  }
+    execute() {
+        uploadForm().then((srcList) => {
+            this.editor.model.change(writer => {
+                const slider = createSlider(writer, srcList);
+                this.editor.model.insertContent(slider);
+            });
+        });
+    }
 
-  refresh() {
-    const model = this.editor.model;
-    const selection = model.document.selection;
-    const allowedIn = model.schema.findAllowedParent(
-      selection.getFirstPosition(),
-      "sliderContainer"
-    );
-
-    this.isEnabled = allowedIn !== null;
-  }
+    // refresh() {
+    //     const model = this.editor.model;
+    //     const selection = model.document.selection;
+    //     const allowedIn = model.schema.findAllowedParent(
+    //         selection.getFirstPosition(),
+    //         "sliderContainer"
+    //     );
+    //     this.isEnabled = allowedIn !== null;
+    // }
 }
 
 function createSlide(writer, src) {
-  const sliderItem = writer.createElement("sliderItem");
-  const sliderItemImage = writer.createElement("sliderItemImage", { src: src });
-  console.log("slider image", sliderItemImage);
-  writer.append(sliderItemImage, sliderItem);
-  return sliderItem;
+    const sliderItem = writer.createElement("sliderItem", {
+        classes: "active"
+    });
+    const sliderItemImage = writer.createElement("sliderItemImage", {
+        src: src
+    });
+    writer.append(sliderItemImage, sliderItem);
+    return sliderItem;
 }
 
-async function createSimpleBox(writer, editor) {
-  console.log("Awqqqq");
-  const sliderContainer = writer.createElement("sliderContainer");
-  const s1 = createSlide(
-    writer,
-    "https://i.pinimg.com/originals/b4/e8/bd/b4e8bd7a4c31ea24748e7a587a110fa1.png"
-  );
-  const s2 = createSlide(
-    writer,
-    "https://66.media.tumblr.com/2a3e8e3433c77abd4660448e0dac45d8/tumblr_nrg3iwtxm91qd31sxo2_r1_1280.gifv"
-  );
-  writer.append(s1, sliderContainer);
-  writer.append(s2, sliderContainer);
-  return sliderContainer;
-  //  const simpleBoxTitle = writer.createElement(uploadFormHtml);
-  const uploadFormHtml = `
-  <div class="slider-inner">
-    <div class="slider-item active">
-      <img src="https://i.pinimg.com/originals/b4/e8/bd/b4e8bd7a4c31ea24748e7a587a110fa1.png" alt="First slide">
-    </div>
-    <div class="slider-item" >
-      <img src="https://66.media.tumblr.com/2a3e8e3433c77abd4660448e0dac45d8/tumblr_nrg3iwtxm91qd31sxo2_r1_1280.gifv" alt="Second slide">
-    </div>
-</div>`;
-
-  const { value: file } = await Swal.fire({
-    title: "Select image",
-    input: "file",
-    inputAttributes: {
-      accept: "image/*",
-      "aria-label": "Upload your profile picture"
-    }
-  });
-  console.log("aee", file);
-
-  if (file == null) {
-    return null;
-  }
-
-  return new Promise(resolve => {
-    const reader = new FileReader();
-    reader.onload = e => {
-      const viewForm = editor.data.processor.toView(uploadFormHtml);
-      const modelForm = editor.data.toModel(viewForm);
-      resolve(modelForm);
-      console.log("aee", e);
-      // Swal.fire({
-      //   title: "Your uploaded picture",
-      //   imageUrl: e.target.result,
-      //   imageAlt: "The uploaded picture"
-      // });
-    };
-    reader.readAsDataURL(file);
-  });
-
-  writer.append(simple, simpleBox);
-  // writer.append(simpleBoxDescription, simpleBox);
-
-  // There must be at least one paragraph for the description to be editable.
-  // See https://github.com/ckeditor/ckeditor5/issues/1464.
-  //writer.appendElement("paragraph", simpleBoxDescription);
+function createSlider(writer, srcList) {
+    const sliderContainer = writer.createElement("sliderContainer");
+    let active = true;
+    srcList.forEach(src => {
+        const slide = createSlide(writer, src, active);
+        writer.append(slide, sliderContainer);
+        active = false;
+    });
+    return sliderContainer;
 }
+
+async function uploadForm() {
+
+    const {value: files} = await Swal.fire({
+        title: "Select image",
+        input: "file",
+        inputAttributes: {
+            "accept": "image/*",
+            "aria-label": "Upload your profile picture",
+            "multiple": true,
+        }
+    });
+
+    const promises = Object.keys(files).map(key => {
+        return new Promise(resolve => {
+            const reader = new FileReader();
+            reader.onload = e => {
+                resolve(e.target.result);
+            };
+            reader.readAsDataURL(files[key]);
+        });
+    });
+
+    const srcList = await Promise.all(promises);
+
+    await Swal.fire({
+        title: "Your uploaded picture",
+        imageUrl: srcList[0],
+        imageAlt: "The uploaded picture"
+    });
+
+    return srcList;
+}
+
+
+//   const s1 = createSlide(
+//     writer,
+//     "https://i.pinimg.com/originals/b4/e8/bd/b4e8bd7a4c31ea24748e7a587a110fa1.png"
+//   );
+//   const s2 = createSlide(
+//     writer,
+//     "https://66.media.tumblr.com/2a3e8e3433c77abd4660448e0dac45d8/tumblr_nrg3iwtxm91qd31sxo2_r1_1280.gifv"
+//   );
+//   writer.append(s1, sliderContainer);
+//   writer.append(s2, sliderContainer);
+//   return sliderContainer;
+//   //  const simpleBoxTitle = writer.createElement(uploadFormHtml);
+//   const uploadFormHtml = `
+//   <div class="slider-inner">
+//     <div class="slider-item active">
+//       <img src="https://i.pinimg.com/originals/b4/e8/bd/b4e8bd7a4c31ea24748e7a587a110fa1.png" alt="First slide">
+//     </div>
+//     <div class="slider-item" >
+//       <img src="https://66.media.tumblr.com/2a3e8e3433c77abd4660448e0dac45d8/tumblr_nrg3iwtxm91qd31sxo2_r1_1280.gifv" alt="Second slide">
+//     </div>
+// </div>`;
+
+
+// writer.append(simple, simpleBox);
+// writer.append(simpleBoxDescription, simpleBox);
+// const viewForm = editor.data.processor.toView(uploadFormHtml);
+// const modelForm = editor.data.toModel(viewForm);
+
+// There must be at least one paragraph for the description to be editable.
+// See https://github.com/ckeditor/ckeditor5/issues/1464.
+//writer.appendElement("paragraph", simpleBoxDescription);
--- src/plugins/simplebox/simpleboxediting.js
+++ src/plugins/simplebox/simpleboxediting.js
@@ -1,106 +1,109 @@
 import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-import {
-  toWidget,
-  toWidgetEditable
-} from "@ckeditor/ckeditor5-widget/src/utils";
-import { modelToViewAttributeConverter } from "@ckeditor/ckeditor5-image/src/image/converters";
+import {toWidget} from "@ckeditor/ckeditor5-widget/src/utils";
 
 import Widget from "@ckeditor/ckeditor5-widget/src/widget";
 import SimpleBoxCommand from "./simpleboxcommand"; // ADDED
 
 export default class SimpleBoxEditing extends Plugin {
-  static get requires() {
-    // ADDED
-    return [Widget];
-  }
-  init() {
-    console.log("SimpleBoxEditing#init() got called");
+    static get requires() {
+        // ADDED
+        return [Widget];
+    }
 
-    this._defineSchema(); // ADDED
-    this._defineConverters();
+    init() {
+        console.log("SimpleBoxEditing#init() got called");
 
-    this.editor.commands.add("SimpleBox", new SimpleBoxCommand(this.editor));
-  }
+        this._defineSchema(); // ADDED
+        this._defineConverters();
 
-  _defineSchema() {
-    // ADDED
-    const schema = this.editor.model.schema;
-    //const model = this.editor.model;
-    schema.register("sliderContainer", {
-      // Behaves like a self-contained object (e.g. an image).
-      isObject: true,
+        this.editor.commands.add("SimpleBox", new SimpleBoxCommand(this.editor));
+    }
 
-      // Allow in places where other blocks are allowed (e.g. directly in the root).
-      allowWhere: "$block"
-    });
+    _defineSchema() {
+        // ADDED
+        const schema = this.editor.model.schema;
+        //const model = this.editor.model;
+        schema.register("sliderContainer", {
+            // Behaves like a self-contained object (e.g. an image).
+            isObject: true,
 
-    schema.register("sliderItem", {
-      // Cannot be split or left by the caret.
-      isLimit: true,
-
-      allowIn: "sliderContainer",
-
-      // Allow content which is allowed in blocks (i.e. text with attributes).
-      allowContentOf: "$root"
-    });
-
-    schema.register("sliderItemImage", {
-      // Cannot be split or left by the caret.
-      isLimit: true,
-
-      allowIn: "sliderItem",
-      allowAttributesOf: "$root",
-
-      allowAttributes: ["alt", "src"],
-      // Allow content which is allowed in the root (e.g. paragraphs).
-      allowContentOf: "$root"
-    });
-  }
-
-  _defineConverters() {
-    // ADDED
-    const conversion = this.editor.conversion;
-
-    conversion.elementToElement({
-      model: "sliderContainer",
-      view: {
-        name: "div",
-        classes: "slider-inner"
-      }
-    });
-
-    conversion.elementToElement({
-      model: "sliderItem",
-      view: {
-        name: "div",
-        classes: "slider-item active"
-      }
-    });
-
-    conversion.elementToElement({
-      model: "sliderItemImage",
-      view: {
-        name: "img",
-        classes: ""
-      }
-    });
-
-    // Then the conversion might be a two way attribute-to-attribute:
-    conversion.attributeToAttribute({
-      model: "src",
-      view: "src"
-    });
-
-    conversion.for("editingDowncast").elementToElement({
-      model: "sliderItemImage",
-      view: (modelElement, viewWriter) => {
-        const img = viewWriter.createAttributeElement("img", {
-          attributes: {
-            src: modelElement._attrs.get("src")
-          }
+            // Allow in places where other blocks are allowed (e.g. directly in the root).
+            allowWhere: "$block"
         });
-        return toWidget(img, viewWriter);
-      }
-    });
-  }
+
+        schema.register("sliderItem", {
+            // Cannot be split or left by the caret.
+            isLimit: true,
+
+            allowIn: "sliderContainer",
+
+            // Allow content which is allowed in blocks (i.e. text with attributes).
+            allowContentOf: "$root"
+        });
+
+        schema.register("sliderItemImage", {
+            // Cannot be split or left by the caret.
+            isLimit: true,
+
+            allowIn: "sliderItem",
+            allowAttributesOf: "$root",
+
+            allowAttributes: ["alt", "src"],
+            // Allow content which is allowed in the root (e.g. paragraphs).
+            allowContentOf: "$root"
+        });
+    }
+
+    _defineConverters() {
+        // ADDED
+        const conversion = this.editor.conversion;
+
+        conversion.elementToElement({
+            model: "sliderContainer",
+            view: {
+                name: "div",
+                classes: "slider-inner"
+            }
+        });
+
+        conversion.elementToElement({
+            model: "sliderItem",
+            view: {
+                name: "div",
+                classes: "slider-item"
+            }
+        });
+
+        conversion.elementToElement({
+            model: "sliderItemImage",
+            view: {
+                name: "img",
+                classes: ""
+            }
+        });
+
+        // Then the conversion might be a two way attribute-to-attribute:
+        conversion.attributeToAttribute({
+            model: "src",
+            view: "src"
+        });
+
+        conversion.attributeToAttribute({
+            model: "alt",
+            view: "alt"
+        });
+
+        conversion.attributeToAttribute({
+            model: "classes",
+            view: "classes"
+        });
+
+        // conversion.for("editingDowncast").elementToElement({
+        //     model: "sliderItemImage",
+        //     view: (modelElement, viewWriter) => {
+        //         const img = viewWriter.createAttributeElement("img");
+        //         return toWidget(img, viewWriter);
+        //     }
+        // });
+    }
 }
